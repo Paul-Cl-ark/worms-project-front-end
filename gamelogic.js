46955c2deeefcd65e4ecb49384350ff413d1a684
Paul-Cl-ark <clark.paul123@gmail.com>
2018-12-30 22:17:53 +0100

    aiming and grenade throwing functions started

--- gamelogic.js
+++ gamelogic.js
@@ -39,7 +39,22 @@ this.aimButton = this.input.keyboard.addKey(Phaser.Keyboard.ENTER)
 this.aimButton.onDown(aim())
 
 function aim () {
+  if (this.grenade.exists) {
+    return
+  }
 
+  //  Re-position the grenade where the worm is
+  this.grenade.reset(this.worm.x, this.worm.y)
+
+  //  Now work out where the END of the target is
+  var p = new Phaser.Point(this.target.x, this.target.y)
+  p.rotate(p.x, p.y, this.target.rotation, false, 34)
+
+  //  So we can see what's going on when the grenade leaves the screen
+  this.camera.follow(this.grenade)
+
+  //  Our launch trajectory is based on the angle of the target and the power
+  this.physics.arcade.velocityFromRotation(this.target.rotation, this.power, this.grenade.body.velocity)
 }
 
 // add spacebar input to throw grenade
@@ -47,6 +62,37 @@ function aim () {
 this.throwButton = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
 this.throwButton.onDown(throwGrenade())
 
+// handles throwing of grenade based on settings in the update method
+
 function throwGrenade () {
 
+  //  If the grenade is in flight we don't let them control anything
+  if (this.grenade.exists) {
+    if (this.grenade.y > 420) {
+      //  Simple check to see if it's fallen too low
+      this.removegrenade()
+    } else {
+      //  grenade vs. the Targets
+      this.physics.arcade.overlap(this.grenade, this.targets, this.hitTarget, null, this)
+    }
+  } else {
+    //  Allow them to set the power between 100 and 600
+    if (this.cursors.left.isDown && this.power > 100) {
+      this.power -= 2
+    } else if (this.cursors.right.isDown && this.power < 600) {
+      this.power += 2
+    }
+
+    //  Allow them to set the angle
+    if (this.cursors.up.isDown) {
+      this.target.angle--
+    } else if (this.cursors.down.isDown && this.target.angle < 0) {
+      this.target.angle++
+    }
+  }
+}
+
+function removegrenade () {
+  this.bullet.kill()
+  this.camera.follow()
 }
